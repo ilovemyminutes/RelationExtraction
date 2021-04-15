0f200314139443136f137c5d4801c1db8d3be9cc
iloveslowfood <silkstaff@naver.com>
2021-04-15 16:03:35 +0000

    configure train pipeline

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ LABELTYPE = "./input/data/label_type.pkl"
 TRAIN = './input/data/train/train.tsv'
 TEST = './input/data/test/test.tsv'
 LABEL = './input/data/label_type.pkl'
+SAVEPATH = './saved_models'
 
 DOT = '.'
 
@@ -19,6 +20,21 @@ class Config:
     Train: str = TRAIN if os.path.isfile(TRAIN) else DOT + TRAIN
     Test: str = TEST if os.path.isfile(TEST) else DOT + TEST
     Label: str = LABEL if os.path.isfile(LABEL) else DOT + LABEL
+    SavePath: str = SAVEPATH if os.path.isfile(SAVEPATH) else DOT + SAVEPATH
+    NumClasses: int = 42
+
+class TrainArgs:
+    Base: dict = dict(
+        num_labels=Config.NumClasses,
+        output_dir=Config.SavePath,
+        overwrite_output_dir=True,
+        num_train_epochs=10,
+        per_device_train_batch_size=32,
+        save_steps=1000,
+        save_total_limit=2,
+        logging_steps=100,
+        seed=42
+    )
     
 @dataclass
 class TokenizerType:
--- model.py
+++ model.py
@@ -1,5 +1,12 @@
-from transformers import BertModel
+from transformers import BertModel, BertConfig
+from config import ModelType
 
 
-def load_model:
-    
+def load_model(pretrained_type: type):
+    if pretrained_type == ModelType.BertMultiLingual:
+        bert_config = BertConfig.from_pretrained(ModelType.BertMultiLingual)
+        model = BertModel(ModelType.BertMultiLingual, config=bert_config)
+    else:
+        raise NotImplementedError()
+    return model
+
