6c200ed880f402525fe63faf3dde0c93a15e1e6b
iloveslowfood <silkstaff@naver.com>
2021-04-16 05:52:45 +0000

    run black

--- evaluation.py
+++ evaluation.py
@@ -3,24 +3,23 @@ import argparse
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 
 
+def compute_metrics(pred, labels):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        y_true=labels, y_pred=preds, average="binary"
+    )
+    acc = accuracy_score(labels, preds)
+
+    return dict(accuracy=acc, f1=f1, precision=precision, recall=recall)
+
+
 def main(args):
     pred = pd.read_csv(args.pred_answer_dir)  # model이 예측한 정답 label
     answer = pd.read_csv(args.public_answer_dir)  # test dataset의 정답 label.
     acc = accuracy_score(list(pred["pred"]), list(answer["answer"]))
     print("accuracy: ", acc)
 
-def compute_metrics(pred, labels):
-    labels = pred.label_ids
-    preds = pred.predictions.argmax(-1)
-    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
-    acc = accuracy_score(labels, preds)
-
-    return dict(
-        accuracy=acc,
-        f1=f1,
-        precision=precision,
-        recall=recall
-        )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
