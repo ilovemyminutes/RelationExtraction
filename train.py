4a830000d9048b3c35d499f2b9026efed245e9ea
iloveslowfood <silkstaff@naver.com>
2021-04-19 04:00:30 +0000

    now can save param dict

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from models import load_model
 from dataset import REDataset, split_train_test_loader
 from optimizers import get_optimizer, get_scheduler
 from criterions import get_criterion
-from utils import get_timestamp, get_timestamp, set_seed, verbose, ckpt_name
+from utils import get_timestamp, get_timestamp, set_seed, verbose, ckpt_name, save_json
 from config import ModelType, Config, Optimizer, PreTrainedType, PreProcessType, Loss
 
 warnings.filterwarnings("ignore")
@@ -25,6 +25,7 @@ def train(
     pretrained_type: str = PreTrainedType.MultiLingual,  # 모델에 활용할 Pretrained BERT Backbone 이름
     num_classes: int = Config.NumClasses,  # 카테고리 수
     pooler_idx: int = 0,  # 인코딩 결과로부터 추출할 hidden state. 0: [CLS]
+    dropout: float = 0.8,
     load_state_dict: str = None,  # (optional) 저장한 weight 경로
     data_root: str = Config.Train,  # 학습 데이터 경로
     preprocess_type: str = PreProcessType.Base,  # 텍스트 전처리 타입
@@ -65,7 +66,7 @@ def train(
 
     # load model
     model = load_model(
-        model_type, pretrained_type, num_classes, load_state_dict, pooler_idx
+        model_type, pretrained_type, num_classes, load_state_dict, pooler_idx, dropout
     )
     model.to(device)
     model.train()
@@ -74,7 +75,9 @@ def train(
     criterion = get_criterion(type=loss_type)
     optimizer = get_optimizer(model=model, type=optim_type, lr=lr)
     if lr_scheduler is not None:
-        scheduler = get_scheduler(type=lr_scheduler, optimizer=optimizer, num_training_steps=TOTAL_STEPS)
+        scheduler = get_scheduler(
+            type=lr_scheduler, optimizer=optimizer, num_training_steps=TOTAL_STEPS
+        )
 
     # make checkpoint directory to save model during train
     checkpoint_dir = f"{model_type}_{pretrained_type}_{TIMESTAMP}"
@@ -94,8 +97,12 @@ def train(
         total_loss = 0
 
         for sentences, labels in tqdm(train_loader, desc="[Train]"):
-            outputs = model(**sentences)
-            loss = criterion(outputs, labels)
+            if model_type == ModelType.VanillaBert:
+                outputs = model(**sentences)
+                loss = criterion(outputs, labels)
+            else:
+                loss, outputs = model(**sentences, labels=labels).values()
+
             total_loss += loss.item()
 
             # backpropagation
@@ -134,8 +141,12 @@ def train(
                     }
                 )
 
+        # evaluate each epoch
         pred_arr = np.hstack(pred_list)
         true_arr = np.hstack(true_list)
+
+        train_eval = evaluate(y_true=true_arr, y_pred=pred_arr)  # ACC, F1, PRC, REC
+        train_loss = total_loss / len(true_arr)
 
         # validation phase
         valid_eval, valid_loss = validate(
@@ -235,14 +246,11 @@ def validate(model, model_type, valid_loader, criterion):
 
     with torch.no_grad():
         for sentences, labels in tqdm(valid_loader, desc="[Valid]"):
-            if model_type == ModelType.SequenceClf:
-                outputs = model(**sentences).logits
-            elif model_type == ModelType.Base:
-                outputs = model(**sentences).pooler_output
-            else:
+            if model_type == ModelType.VanillaBert:
                 outputs = model(**sentences)
-
-            loss = criterion(outputs, labels)
+                loss = criterion(outputs, labels)
+            else:
+                loss, outputs = model(**sentences, labels=labels).values()
             total_loss += loss.item()
 
             _, preds = torch.max(outputs, dim=1)
@@ -269,28 +277,31 @@ if __name__ == "__main__":
     LOAD_STATE_DICT = None
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model-type", type=str, default=ModelType.VanillaBert)
+    parser.add_argument("--model-type", type=str, default=ModelType.SequenceClf)
     parser.add_argument(
         "--pretrained-type", type=str, default=PreTrainedType.MultiLingual
     )
     parser.add_argument("--num-classes", type=int, default=Config.NumClasses)
     parser.add_argument("--pooler-idx", type=int, default=0)
+    parser.add_argument("--dropout", type=float, default=0.8)
     parser.add_argument("--load-state-dict", type=str, default=LOAD_STATE_DICT)
     parser.add_argument("--data-root", type=str, default=Config.Train)
     parser.add_argument("--preprocess-type", type=str, default=PreProcessType.ES)
     parser.add_argument("--epochs", type=int, default=Config.Epochs)
     parser.add_argument("--valid-size", type=int, default=Config.ValidSize)
-    parser.add_argument("--train-batch-size", type=int, default=Config.Batch32)
+    parser.add_argument("--train-batch-size", type=int, default=Config.Batch8)
     parser.add_argument("--valid-batch-size", type=int, default=512)
     parser.add_argument("--optim-type", type=str, default=Optimizer.Adam)
-    parser.add_argument("--loss-type", type=str, default=Loss.CE)
-    parser.add_argument("--lr", type=float, default=Config.LRSlower)
+    parser.add_argument("--loss-type", type=str, default=Loss.LS)
+    parser.add_argument("--lr", type=float, default=Config.LRSlow)
     parser.add_argument("--lr-scheduler", type=str, default=Optimizer.CosineAnnealing)
     parser.add_argument("--device", type=str, default=Config.Device)
     parser.add_argument("--seed", type=int, default=Config.Seed)
     parser.add_argument("--save-path", type=str, default=Config.CheckPoint)
 
     args = parser.parse_args()
+    if args.model_type == ModelType.SequenceClf:
+        args.loss_type = Loss.CE
 
     # register logs to wandb
     name = (
@@ -299,8 +310,21 @@ if __name__ == "__main__":
     run = wandb.init(project="pstage-klue", name=name, reinit=True)
     wandb.config.update(args)
 
+    # make checkpoint directory to save model during train
+    checkpoint_dir = f"{args.model_type}_{args.pretrained_type}_{TIMESTAMP}"
+    if checkpoint_dir not in os.listdir(args.save_path):
+        os.mkdir(os.path.join(args.save_path, checkpoint_dir))
+    args.save_path = os.path.join(args.save_path, checkpoint_dir)
+
+    # save param dict
+    save_param = vars(args)
+    save_param['device'] = save_param['device'].type
+    save_json(os.path.join(args.save_path, 'param_dict.json'), save_param)
+
     # train
-    TOTAL_STEPS = args.epochs * (int(TOTAL_SAMPLES * (1-args.valid_size)) // args.train_batch_size)
+    TOTAL_STEPS = args.epochs * (
+        int(TOTAL_SAMPLES * (1 - args.valid_size)) // args.train_batch_size
+    )
     print("=" * 100)
     print(args)
     print("=" * 100)
