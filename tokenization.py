252da47813ff7d4d1a50ff24a7eb5db281cfd515
iloveslowfood <silkstaff@naver.com>
2021-04-19 06:27:05 +0000

    update tokenization

--- tokenization.py
+++ tokenization.py
@@ -2,6 +2,12 @@ from dataclasses import dataclass
 from transformers import BertTokenizer, AutoTokenizer
 from config import PreProcessType, PreTrainedType
 
+# 토큰화 결과 [CLS] 토큰이 가장 앞에 붙게 되기 떄문에
+# Entity Mark에 대한 임베딩 값을 조절하는 과정에서 인덱스를 OFFSET만큼 밀어주기 위해 사용
+OFFSET = 1
+ENTITY_SCORE = 2
+SEP_SCORE = 2
+
 
 @dataclass
 class SpecialToken:
@@ -9,17 +15,73 @@ class SpecialToken:
     SEP: str = "[SEP]"
     CLS: str = "[CLS]"
 
+    # 무엇(entity)은 무엇(entity)과 어떤 관계이다.(순서 고려 X)
+    EOpen: str = "[ENT]"
+    EClose: str = "[/ENT]"
+
     # 무엇(entity1)은 무엇(entity2)과 어떤 관계이다.
     E1Open: str = "[E1]"
     E1Close: str = "[/E1]"
     E2Open: str = "[E2]"
     E2Close: str = "[/E2]"
 
-    # 무엇(sub)은 무엇(obj)과 어떤 관계이다.
-    SUBOpen: str = "[SUB]"
-    SUBClose: str = "[/SUB]"
-    OBJOpen: str = "[OBJ]"
-    OBJClose: str = "[/OBJ]"
+
+def tokenize(sentences, tokenizer, preprocess_type: str):
+    outputs = tokenizer(
+        sentences,
+        return_tensors="pt",
+        padding=True,
+        truncation=True,
+        max_length=100,
+        add_special_tokens=True,
+    )
+    if preprocess_type != PreProcessType.Base:
+        tokenized = tokenizer.tokenize(sentences)
+
+        if preprocess_type == PreProcessType.EM:
+            # Add embedding value for entity marker tokens([E1], [/E1], [E2], [/E2])
+            entity_indices = find_entity_indices(tokenized)
+            for open, close in entity_indices.values():
+                outputs.token_type_ids[
+                    OFFSET + open : OFFSET + close + 1
+                ] += ENTITY_SCORE
+
+        elif preprocess_type == PreProcessType.ESP:
+            # Add embedding value for separation token([SEP])
+            last_sep_idx = fine_sep_indices(tokenized).pop()
+            outputs.token_type_ids[OFFSET : last_sep_idx + 1] += SEP_SCORE
+            return outputs
+
+        elif preprocess_type == PreProcessType.EMSP:
+            entity_indices = find_entity_indices(tokenized)
+            for (open, close) in entity_indices.values():
+                outputs.token_type_ids[
+                    OFFSET + open : OFFSET + close + 1
+                ] += ENTITY_SCORE
+
+            last_sep_idx = fine_sep_indices(tokenized).pop()
+            outputs.token_type_ids[OFFSET : last_sep_idx + 1] += SEP_SCORE
+
+    return outputs
+
+
+def find_entity_indices(tokenized: list) -> dict:
+    entity_indices = {
+        "e1": (
+            tokenized.index(SpecialToken.E1Open),
+            tokenized.index(SpecialToken.E1Close),
+        ),
+        "e2": (
+            tokenized.index(SpecialToken.E1Open),
+            tokenized.index(SpecialToken.E1Close),
+        ),
+    }
+    return entity_indices
+
+
+def fine_sep_indices(tokenized: list) -> list:
+    sep_indices = [idx for idx, tok in enumerate(tokenized) if tok == SpecialToken.SEP]
+    return sep_indices
 
 
 def load_tokenizer(type: str = PreProcessType.Base):
@@ -35,6 +97,20 @@ def load_tokenizer(type: str = PreProcessType.Base):
     print("Load Tokenizer...", end="\t")
     if type in [PreProcessType.Base, PreProcessType.ES, PreProcessType.ESP]:
         tokenizer = BertTokenizer.from_pretrained(PreTrainedType.MultiLingual)
+
+    # Entity Marker, Entity Marker Separator with Position Embedding
+    elif type in [PreProcessType.EM, PreProcessType.EMSP]:
+        tokenizer = BertTokenizer.from_pretrained(PreTrainedType.MultiLingual)
+        tokenizer.add_special_tokens(
+            {
+                "additional_special_tokens": [
+                    SpecialToken.E1Open,
+                    SpecialToken.E1Close,
+                    SpecialToken.E2Open,
+                    SpecialToken.E2Close,
+                ]
+            }
+        )
     else:
         raise NotImplementedError
     print("done!")
